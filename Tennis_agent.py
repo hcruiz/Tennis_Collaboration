191fb5013758c923773be03d3b986e9187496adb
HCR <hans.ruiz.euler@hotmail.com>
2019-08-05 09:01:12 +0200

    first commit; working version, needs parameter tuning

---
+++ Tennis_agent.py
@@ -0,0 +1,122 @@
+'''
+'''
+import torch
+import torch.nn as nn
+from ACNets import ActorNet, CriticNet
+from torch.optim import Adam
+import numpy as np
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print("using device: ",device)
+
+class SelfPlay_Agent(nn.Module):
+    
+    def __init__(self, state_size, action_size, num_agents, discount=0.99, tau=0.01, lr_act=2.e-4, lr_crit=2.e-4):
+        
+        super(SelfPlay_Agent, self).__init__()
+        
+        self.num_agents = num_agents
+        self.discount = discount
+        self.tau = tau
+        self.lr_act = lr_act
+        self.lr_crit = lr_crit
+        
+        # Define Actor and Critic Networks
+        self.actor = ActorNet(state_size, action_size).to(device)
+        self.actor_target = ActorNet(state_size, action_size).to(device)
+        
+        self.critic = CriticNet(state_size, action_size, num_agents).to(device)
+        self.critic_target = CriticNet(state_size, action_size, num_agents).to(device)
+        
+        for param in self.actor_target.parameters():
+            param.requires_grad = False
+        for param in self.actor_target.parameters():
+            param.requires_grad = False
+            
+        # Copy AC networks to target networks
+        self.copy2target(self.actor,self.actor_target)
+        self.copy2target(self.critic,self.critic_target)
+        
+        # Initialize Optimizers
+        self.actor_optim = Adam(self.actor.parameters(), lr=self.lr_act)
+        self.critic_optim = Adam(self.critic.parameters(), lr=self.lr_crit)
+        # Define loss functions
+        self.critic_loss = nn.MSELoss()
+
+    def optim_step(self,loss,optim):
+        optim.zero_grad()
+        loss.backward()
+        optim.step()
+        
+    ############### Critic updater ########################
+    def update_critic(self, minibatch, agent_i):
+        q_inputs = self.get_qinputs(minibatch)
+        qtarget_inputs = self.get_qtarget_next(minibatch)
+        gamma_Qnext = self.discount*self.critic_target(qtarget_inputs)
+        q_vals = self.critic(q_inputs).view(-1)
+        rewards = torch.tensor([minibatch[i][2][agent_i] for i in range(len(minibatch))], dtype=torch.float, device=device)
+        dones = [torch.tensor(np.asarray(minibatch[i][-1])[np.newaxis],dtype=torch.float, device=device) for i in range(len(minibatch))]
+        dones = torch.cat(dones,dim=0)
+        y = rewards + gamma_Qnext.view(-1)*(1-dones[:,agent_i])
+        loss = self.critic_loss(q_vals,y)
+        self.optim_step(loss, self.critic_optim)
+        return loss.data.cpu().numpy()
+    
+    def get_qinputs(self,batch):
+        state_batch = [torch.tensor(batch[i][0].flatten()[np.newaxis,:],dtype=torch.float,device=device) for i in range(len(batch))]
+        state_batch = torch.cat(state_batch,dim=0)
+        actions_batch = [torch.tensor(batch[i][1].flatten()[np.newaxis,:],dtype=torch.float,device=device) for i in range(len(batch))]
+        actions_batch = torch.cat(actions_batch,dim=0)
+        return torch.cat([state_batch,actions_batch],dim=1)
+    
+    def get_qtarget_next(self,batch):
+        state_batch = [torch.tensor(batch[i][-2][np.newaxis],dtype=torch.float,device=device) for i in range(len(batch))]
+        state_batch = torch.cat(state_batch,dim=0)
+        actions_batch = [self.actor_target(state_batch[:,0]),self.actor_target(state_batch[:,1])]
+        actions_batch = torch.cat(actions_batch,dim=1)
+        state_batch = state_batch.view(len(batch),-1)
+        target_inputs = torch.cat([state_batch,actions_batch],dim=1)
+        return target_inputs
+    
+    ############### Actor updater ########################
+    def update_actor(self,minibatch,agent_i):
+        state_batch, actions_batch = self.get_states_actions(minibatch)
+        actions_batch[:,agent_i] = self.actor(state_batch[:,agent_i])
+        state_batch = state_batch.view(len(minibatch),-1)
+        actions_batch = actions_batch.view(len(minibatch),-1)
+        q_inputs = torch.cat([state_batch,actions_batch],dim=1)
+        loss = -self.critic(q_inputs).mean()
+        self.optim_step(loss, self.actor_optim)    
+        return loss.data.cpu().numpy()
+    
+    def get_states_actions(self,batch):
+        state_batch = [torch.tensor(batch[i][0][np.newaxis],dtype=torch.float,device=device) for i in range(len(batch))]
+        state_batch = torch.cat(state_batch,dim=0)
+        actions_batch = [torch.tensor(batch[i][1][np.newaxis],dtype=torch.float,device=device) for i in range(len(batch))]
+        actions_batch = torch.cat(actions_batch,dim=0)
+        return state_batch, actions_batch
+    #######################################################
+    def target_update(self):
+        # Soft update ACTOR
+        target_actor_params = self.actor_target.parameters()
+        actor_params = self.actor.parameters()
+        for target_params, params in zip(target_actor_params,actor_params): 
+            target_params.data.copy_(self.tau*params.data+(1.-self.tau)*target_params.data)
+        # Soft update CRITIC
+        target_critic_params = self.critic_target.parameters()
+        critic_params = self.critic.parameters()
+        for target_params, params in zip(target_critic_params,critic_params):
+            target_params.data.copy_(self.tau*params.data+(1-self.tau)*target_params.data)
+    
+    def copy2target(self, network, target):
+        target_params = target.parameters()
+        network_params = network.parameters()
+        for t_params, n_params in zip(target_params,network_params): 
+            t_params.data.copy_(n_params.data)
+
+    def act(self, state):
+        
+        x = torch.tensor(state, dtype=torch.float, device=device)
+        
+        return self.actor(x)
+   
---
+++ utils.py
@@ -0,0 +1,42 @@
+'''Contains helper functions for MAPPGD to solve the collaboration Tennis project.
+'''
+import numpy as np
+import torch
+from collections import deque
+import random
+
+class OUprocess:
+    '''Addapted from https://github.com/songrotek/DDPG/blob/master/ou_noise.py
+    Here, the OU process is centered at the origin so no mu parameter required
+    '''
+    def __init__(self, action_dimension, theta=0.15, sigma=1.0):
+        self.action_dimension = action_dimension
+        self.theta = theta
+        self.sigma = sigma
+        self.reset()
+
+    def reset(self):
+        self.state = np.zeros(self.action_dimension)
+
+    def noise(self):
+        dx = - self.theta * self.state + self.sigma * np.random.randn(len(self.state))
+        self.state = self.state + dx
+        return self.state     
+    
+    
+class Reply_buffer:
+    def __init__(self, buffer_size):
+        self.buffer = deque(maxlen=buffer_size)
+        self.buffer_size = buffer_size
+        
+    def store(self, samples):
+#         for sars in list(zip(*samples)):
+#             self.buffer.append(sars)
+        self.buffer.append(samples)
+        
+    def get_batch(self,batch_size):
+        '''get list of (s,a,r,s') tuples of length batch_size
+        '''
+        batch = random.sample(self.buffer,batch_size)
+        return batch
+    
